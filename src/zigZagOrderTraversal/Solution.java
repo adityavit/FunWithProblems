5a41d6855e6a52f67eff44dcc73ee8b969de7f78
aditya bhatia <adib@yahoo-inc.com>
2015-07-22 01:44:05 -0700

    Adding ZigZag order traversal

---
+++ src/zigZagOrderTraversal/Solution.java
@@ -0,0 +1,15 @@
+package zigZagOrderTraversal;
+
+
+import isBST.TreeNode;
+
+import java.util.List;
+
+/**
+ * Created by adib on 7/22/15.
+ */
+public class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        //BFS use a Deque as the queue.
+    }
+}
